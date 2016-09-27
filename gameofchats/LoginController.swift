2c2518b30f2ce37506bf2543ca5a1e6e4541ff30
Alex Lorenzo <Alexanderlore87@gmail.com>
2016-09-27 11:43:07 -0400

    added Login

---
+++ gameofchats/LoginController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginController.swift
+//  gameofchats
+//
+//  Created by alexander lorenzo on 9/27/16.
+//  Copyright © 2016 Bruh Incorporated. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    
+}
